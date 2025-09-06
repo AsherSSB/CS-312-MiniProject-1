479bf6a0532caa2a3877a3905d6877c91110bf4a
AsherSSB <gromanenghi04@gmail.com>
2025-09-06 16:51:23 -0700

    created and linked static folder to project

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-const homeRoutes = require("./app/routes/homeRoutes");
+const homeRoutes = require('./app/routes/homeRoutes');
 
-app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'app/views'));
 
 app.use('/', homeRoutes);
+app.use(express.static(path.join(__dirname, 'static')));
 
 module.exports = app;
