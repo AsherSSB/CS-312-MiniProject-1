0eb9ab1121c3beb403bea191c4eff152dab0592d
AsherSSB <gromanenghi04@gmail.com>
2025-09-10 10:53:57 -0700

    adds category creation to router

--- app/lib/BlogPost.js
+++ app/lib/BlogPost.js
@@ -1,9 +1,10 @@
 class BlogPost {
-    constructor(id, author, title, content) {
+    constructor(id, author, title, content, category) {
         this.id = id;
         this.author = author;
         this.title = title;
         this.content = content;
+		this.category = category;
         this.time = new Date();
     }
 }
